3b687f9db693c8b92bfb258d9c4635f8b856d708
Gark <artem.garkusha@gmail.com>
2013-03-17 10:03:47 +0200

    analitic + admob + icon

--- cockroach/src/gark/tap/cockroach/GameActivity.java
+++ cockroach/src/gark/tap/cockroach/GameActivity.java
@@ -4,6 +4,8 @@ import gark.tap.cockroach.mathengine.MathEngine;
 import gark.tap.cockroach.mathengine.Utils;
 import android.graphics.Typeface;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 public class GameActivity extends MainActivity {
 	private MathEngine mMathEngine;
 
@@ -51,5 +53,17 @@ public class GameActivity extends MainActivity {
 			e.printStackTrace();
 		}
 		super.onPause();
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		EasyTracker.getInstance().activityStart(this); 
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		EasyTracker.getInstance().activityStop(this);
 	}
 }
